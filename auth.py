344948f5162f1ad0cf993165858987f965131e30
sutt <wsutton17@gmail.com>
2025-10-14 18:22:25 -0400

    fix: resolve bcrypt password length and dependency errors

--- auth.py
+++ auth.py
@@ -53,7 +53,7 @@ def verify_password(plain_password, hashed_password):
 
 def get_password_hash(password):
     """Hashes a password."""
-    return pwd_context.hash(password)
+    return pwd_context.hash(password.encode('utf-8')[:72])
 
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
